c2d705ac813073f5d8bcffd8d156ed016697f3ee
Daniel Hellstern <dhellstern@outlook.com>
2021-05-12 11:08:43 +0000

    Completely revamped linking to better support real-world transcoding structures
    Removed redundant code for local and remote execution

--- frt.py
+++ frt.py
@@ -7,6 +7,7 @@ import signal
 import uuid
 import sys
 import os
+import time
 import re
 
 # Create a job identifier (used to uniquely identify files)
@@ -18,6 +19,13 @@ log = logging.getLogger(f"ffmpeg-remote-transcoder-{job[:6]}")
 # Load the ffmpeg-remote-transcoder configuration
 config = configparser.ConfigParser()
 config.read("/etc/frt.conf")
+
+# Create the local working directory
+localdir = os.path.join(config.get("Client", "WorkingDirectory", fallback="/opt/frt/"), job)
+os.makedirs(localdir)
+
+# Predict the remote mounted working directory
+remotedir = os.path.join(config.get("Server", "WorkingDirectory"), job)
 
 # Validate that the required parameters are set
 required_params = (("Server", "Host"), ("Server", "Username"), ("Server", "WorkingDirectory"))
@@ -32,10 +40,6 @@ ffmpeg_args = sys.argv[1:]
 # Commands that should be redirected to stdout
 commands_bypass = { "-help", "-h", "-version", "-encoders", "-decoders" }
 bypass = len([ cmd for cmd in commands_bypass if cmd in ffmpeg_args ]) > 0
-
-# Linked files to dereference later
-src_link = ()
-dest_link = ()
 
 def generate_ssh_command():
     """
@@ -72,51 +76,57 @@ def generate_ssh_command():
     
     return ssh_command
 
-def convert_references(ffmpeg_command, dir):
+def forward_reference(ffmpeg_command):
     """
-    Convert file references to temporary job symlinks
+    Link source files to the working directory
 
-    :param ffmpeg_command: The ffmpeg command to convert
-    :param dir: The server working directory to reference links towards
+    :param ffmpeg_command: The ffmpeg command to parse
     """
-    global src_link
-    global dest_link
+    # Find and replace all file references with links
+    for i, arg in enumerate(ffmpeg_command):
+        # If the argument appears to have a file extension
+        if len(os.path.splitext(arg)[1]) > 1:
+            absolute = os.path.abspath(arg)
 
-    localdir = config.get("Client", "WorkingDirectory", fallback="/opt/frt/")
+            relative = os.path.relpath(absolute, "/")
 
-    # Convert the job reference to point to the infile
-    if "-i" in ffmpeg_command:
-        inindex = ffmpeg_command.index("-i") + 1
-        infile = ffmpeg_command[inindex]
+            local_working = os.path.join(localdir, relative)
+            remote_working = os.path.join(remotedir, relative)
 
-        if not infile.startswith("pipe:"):
-            # Link up the source file
-            ext = os.path.splitext(infile)
-            name = f"{job}-src{ext[1]}"
+            # Create all directories in the path
+            os.makedirs(os.path.dirname(local_working), exist_ok=True)
 
-            # Link the files and add them to the cleanup stack
-            src_link = (os.path.abspath(infile), os.path.join(localdir, name))
-            link(*src_link)
+            # Link source files properly
+            if ffmpeg_command[i - 1] == "-i" and not os.path.islink(local_working):
+                os.symlink(absolute, local_working)
 
-            # Replace the infile reference
-            ffmpeg_command[inindex] = os.path.join(dir, name)
+                log.info(f"Linked source file {absolute}")
 
-    outfile = ffmpeg_command[-1]
+            # Replace paths with adjusted remote working paths
+            ffmpeg_command[i] = remote_working
 
-    # Convert the outfile reference to point to the job file
-    if len(ffmpeg_command) > 1 and not bypass and not outfile.startswith("pipe:"):
-        # Link up the destination file
-        ext = os.path.splitext(outfile)
-        name = f"{job}-dest{ext[1]}"
+            # Note that no links are made for destination files as these are detected and linked at runtime
 
-        # Link the files and add them to the cleanup stack
-        dest_link = (os.path.join(localdir, name), os.path.abspath(outfile))
-        link(*dest_link)
+def reverse_reference():
+    """
+    Detects and links output files from ffmpeg to their final destination
+    """
+    for root, _, files in os.walk(localdir):
+        for file in files:
+            # Ignore infile references
+            if not os.path.islink(file):
+                relative = os.path.relpath(os.path.join(root, file), localdir)
+                absolute = os.path.join("/", relative)
+                working = os.path.join(root, file)
 
-        # Replace the outfile reference
-        ffmpeg_command[-1] = os.path.join(dir, name)
+                # Test if the file is already linked
+                if not os.path.islink(absolute):
+                    # Link the destination output to the working copy
+                    os.symlink(working, absolute)
 
-def generate_ffmpeg_command(context="Server"):
+                    log.info(f"Linked destination file {absolute}")
+
+def generate_ffmpeg_command(context):
     """
     Generate a properly escaped and transformed ffmpeg commandline
 
@@ -161,77 +171,44 @@ def map_std(ffmpeg_command):
     
     return (stdin, stdout, stderr)
 
-def link(source, destination):
+def run_ffmpeg_command(context="Server"):
     """
-    Link the source and destination files to the working directory
+    Run the ffmpeg command, remapping I/O as necessary
 
-    :param source: The source file to symlink
-    :param destination: The destination symlink location
-
-    :returns: The filename of the newly created link
-    """
-    name = os.path.basename(destination)
-
-    # Create or reuse the link
-    if not os.path.islink(destination):
-        # Link the file
-        os.symlink(source, destination)
-
-        log.info(f"Created link {name}")
-    else:
-        log.info(f"Using existing link {name}")
-
-def run_ffmpeg_remote():
-    """
-    Run the ffmpeg command on the remote host, handing I/O as necessary
-
+    :param context: Whether to run the command on the server or the client
     :returns: The return code from the ffmpeg process
     """
     ssh_command = generate_ssh_command()
-    ffmpeg_command = generate_ffmpeg_command()
-
-    # Predict the location of shared files on the server
-    dir = config.get("Server", "WorkingDirectory")
-    convert_references(ffmpeg_command, dir)
+    ffmpeg_command = generate_ffmpeg_command(context)
 
     # Remap the standard in, out, and error to properly handle data streams
     (stdin, stdout, stderr) = map_std(ffmpeg_command)
 
-    log.info("Running ffmpeg command on remote server...")
+    # Update file links and prepare working directory
+    forward_reference(ffmpeg_args)
+
+    log.info(f"Running ffmpeg command on {context.lower()}...")
     log.info(ffmpeg_command)
 
-    # Run the command on the remote host
-    proc = subprocess.run(ssh_command + ffmpeg_command, shell=False, bufsize=0, universal_newlines=True, stdin=stdin, stdout=stdout, stderr=stderr)
+    # Run the ffmpeg command
+    if context == "Server":
+        command = ssh_command + ffmpeg_command
+    elif context == "Client":
+        command = ffmpeg_command
+    proc = subprocess.Popen(command, shell=False, bufsize=0, universal_newlines=True, stdin=stdin, stdout=stdout, stderr=stderr)
+
+    # Wait until the command has finished, while checking for new output files
+    while proc.poll() is None:
+        # Walk the directory, building reverse references for new files
+        reverse_reference()
+
+        time.sleep(0.25)
 
     # Fall back to local ffmpeg if SSH could not connect
-    if proc.returncode == 255:
+    if context == "Server" and proc.returncode == 255:
         log.error("Failed to connect to remote host")
-        return run_ffmpeg_local()
+        return run_ffmpeg_command(context="Client")
     
-    # Return the ffmpeg return code
-    return proc.returncode
-
-def run_ffmpeg_local():
-    """
-    Run the ffmpeg command on the local host, handling I/O as necessary
-
-    :returns: The return code from the ffmpeg process
-    """
-    ffmpeg_command = generate_ffmpeg_command(context="Client")
-
-    # Link files to the working directory
-    dir = config.get("Client", "WorkingDirectory", fallback="/opt/frt/")
-    convert_references(ffmpeg_command, dir)
-
-    # Remap the standard in, out, and error to properly handle data streams
-    (stdin, stdout, stderr) = map_std(ffmpeg_command)
-
-    log.info("Running ffmpeg command local host...")
-    log.info(ffmpeg_command)
-
-    # Run the command
-    proc = subprocess.run(ffmpeg_command, shell=False, bufsize=0, universal_newlines=True, stdin=stdin, stdout=stdout, stderr=stderr)
-
     # Return the ffmpeg return code
     return proc.returncode
 
@@ -252,18 +229,22 @@ def cleanup(signum="", frame=""):
     
     subprocess.run(ssh_command + kill_command)
     
-    # Remove the link from the source file to a working reference
-    if len(src_link) == 2:
-        os.unlink(src_link[1])
+    log.info("Unlinking file references...")
+    for root, _, files in os.walk(localdir, topdown=False):
+        for file in files:
+            if os.path.islink(file):
+                # Remove symbolic links but do not remove the source
+                os.unlink(file)
+            else:
+                relative = os.path.relpath(os.path.join(root, file), localdir)
+                absolute = os.path.join("/", relative)
+                working = os.path.join(root, file)
 
-        log.info("Unlinked source file")
+                # Move the completed file to its destination, replacing any links
+                os.replace(working, absolute)
 
-    # Remove the link from the reference to the working destination
-    if len(dest_link) == 2:
-        # Move the completed file from the working directory to the place where the link resided
-        os.replace(*dest_link)
-
-        log.info("Unlinked and moved destination file")
+        # Remove the current directory (walking starts from the lowest level)
+        os.rmdir(root)
     
     log.info("Cleaned up, exiting")
 
@@ -280,10 +261,10 @@ def main():
     logfile = config.get("Logging", "LogFile", fallback="/var/log/frt.log")
     logging.basicConfig(filename=logfile, level=logging.INFO)
 
-    log.info("Beginning remote transcoding...")
+    log.info("Beginning transcoding...")
 
     # Run ffmpeg on the remote host
-    status = run_ffmpeg_remote()
+    status = run_ffmpeg_command()
 
     if status == 0:
         log.info(f"ffmpeg finished with return code {status}")
